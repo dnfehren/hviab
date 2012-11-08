294a008f0fe4d6784e0b05c082990f1b7c392fbc
Daniel Fehrenbach <dnfehren@simple.(none)>
2012-11-07 20:16:52 -0600

    need to work on this later

--- builders/abandoned_buildings.py
+++ builders/abandoned_buildings.py
@@ -4,26 +4,25 @@ from local_dev_help.scripts import consolelog
 
 def list_all():
 
-    v_header = []
     v_data = []
+
+    def make_adr_id(raw_adr):
+        adr_id = raw_adr.replace(" ", "_").lower().replace(".","")
+        return adr_id
 
     with open('./data_src/vacants_latest.csv') as vacants_csv:
         v_reader = csv.reader(vacants_csv)
 
         for row_num, row in enumerate(v_reader):
-            if row_num == 0:
-                                
-                for row_part in row:
-                    col_head = {'sTitle':row_part}
-                    v_header.append(col_head)
 
-            else:
-                row.append(row[19].replace(" ", "_").lower().replace(".",""))
-                v_data.append(row)
+            out_row = []
+            out_row.append(row[19]) #full address
+            out_row.append(row[18]) #zip
+            out_row.append(row[25]) #lat
+            out_row.append(row[26]) #lon
+            out_row.append(make_adr_id(row[19]))
+            v_data.append(out_row)
 
-    head = {'titles':v_header}
-    data = {'data':v_data}
-
-    data_container = {"abd_bldgs" : [head, data]} 
+    data_container = {'aaData':v_data}
     
     return data_container
